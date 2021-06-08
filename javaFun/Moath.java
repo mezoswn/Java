8f96bf4319eaa0d0567baccf95a4f0d0f755b4cd
Moath <m.sweidan@hotmail.com>
2021-06-09 01:49:30 +0300

    First Java Program

---
+++ javaFun/Moath.java
@@ -0,0 +1,7 @@
+public class Moath {
+    public static void main(String[] args) {
+        System.out.println("My name is Moath");
+		System.out.println("I am 34 years old");
+		System.out.println("My home town is Nablus,Palestine");
+    }
+}
